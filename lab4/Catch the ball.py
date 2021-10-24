196516b28786cef618224fbd427ac3b8e2849aa1
Bcrfntkm <vasiutin.pa@phystech.edu>
2021-10-24 18:50:22 +0300

    New kind of objects added. Data is still saving into txt file (I didn't understand how to use yaml or json format)

--- lab4/Catch the ball.py
+++ lab4/Catch the ball.py
@@ -2,12 +2,15 @@ import pygame
 from pygame.draw import *
 from random import randint
 
-print('Choose your level: Elementary, Confidence, Veteran')
-level = str(input())
-levels = {'Elementary': 1.5,
-          'Confidence': 1,
-          'Veteran': 0.5
-         }
+time = 0
+pool = []
+
+print('Choose your level: Elementary(1), Confidence(2), Veteran(3)')
+level = input()
+levels = {'1': 1.5,
+          '2': 1,
+          '3': 0.5
+          }
 
 quot = levels[level]
 
@@ -20,21 +23,22 @@ result = 0
 
 class Ball:
     '''
-    Class that make and move the balls
+    Class that makes and moves the balls
     '''
     def __init__(self):
         '''
         Function that put the ball on a random place with random velocity
         '''
         self.coord = [randint(100, 1100), randint(100, 900)]
-        self.r = randint(30, 100) * quot
-        self.velocity = [randint(-30, 30) / quot, randint(-30, 30) / quot]
+        self.r = randint(60, 100) * quot
+        self.velocity = [randint(-25, 25) / quot, randint(-25, 25) / quot]
         self.color = (randint(0, 254), randint(0, 254), (randint(0, 254)))
         self.flag = True
+        self.points = 100//self.r
 
-    def move(self):
+    def move(self, time):
         '''
-        Function that moves th ball
+        Function that moves the ball
         '''
         v0_x, v0_y = self.velocity
         self.coord[0] += v0_x
@@ -66,20 +70,69 @@ class Ball:
         if (pos_x-x0)**2 + (pos_y-y0)**2 <= self.r**2:
             self.flag = False
 
+class Square:
+    '''
+    Class that creates the fluctuating square
+    '''
+    def __init__(self):
+        self.r = randint(80, 100) * quot
+        self.coord = [randint(100, 1100), randint(100, 900)]
+        self.color = (randint(0, 254), randint(0, 254), (randint(0, 254)))
+        self.flag = True
+        self.points = 200//self.r
+
+    def move(self, time):
+        '''
+        Function that moves the square
+        time: counter of moves
+        '''
+        if time % (15*quot) == 0:
+            self.coord = [randint(100, 1100), randint(100, 900)]
+
+    def draw(self):
+        '''
+        Function draws the square via its coordinates
+        '''
+        rect(screen, self.color, [self.coord[0], self.coord[1], self.r, self.r])
+
+    def event(self, event):
+        '''
+        Function handles the event that a gamer make
+        event: here is press of mouse button
+        '''
+        x0, y0 = self.coord
+        pos_x, pos_y = event.pos
+        if (abs(x0 + self.r / 2 - pos_x) < self.r) and (abs(y0 + self.r / 2 - pos_y < self.r)):
+            self.flag = False
+
+    def collision(self, BORDER_OF_EDGE):
+        '''
+        Function do nothing as square cannot make a collision
+        :param BORDER_OF_EDGE: borders of game window
+        '''
+        pass
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-
-pool = [Ball(), Ball(), Ball(), Ball(), Ball()]
+for _ in range(int(3/quot)):
+    pool.append(Ball())
+    pool.append(Ball())
+    pool.append(Square())
 
 while not finished:
     clock.tick(FPS)
 
+    time += 1
+
     for var in pool:
-        var.move()
+        var.move(time)
         var.draw()
         var.collision([1200, 900])
+        if time % 5 == 0:
+            var.next_move = True
+
     for event in pygame.event.get():
         for var in pool:
             if event.type == pygame.QUIT:
@@ -88,7 +141,7 @@ while not finished:
                 var.event(event)
                 if not var.flag:
                     pool.pop(pool.index(var))
-                    result += 100//var.r
+                    result += var.points
     if not pool:
         finished = True
 
@@ -96,6 +149,8 @@ while not finished:
     screen.fill(BLACK)
 
 pygame.quit()
+
+result *= 9*FPS/time
 
 print('Enter your name:')
 name = input()
